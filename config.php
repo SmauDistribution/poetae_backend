2d4da7ef207e77e97f9ab00fbddf577b272ba61e
Daniel Smau <smaudaniel19@gmail.com>
2021-04-25 19:14:34 +0200

    Aggiunto la configurazione

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+class Connect extends PDO
+{
+    public function __construct() {
+        parent::__construct("mysql:host=localhost;dbname=poetaedb", "root", "root1234");
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Nel caso di errori, usa le eccezioni 
+        $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+}
+
+class Options {
+    CONST CROSS_ORIGIN = 'Access-Control-Allow-Origin: *';
+}
+
+?>
