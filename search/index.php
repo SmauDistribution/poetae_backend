02820c2a38a27da149a8b83e64ac990b180c792b
Daniel Smau <smaudaniel19@gmail.com>
2021-04-25 21:03:12 +0200

    Aggiunto l' endpoint search senza le immagini

---
+++ search/index.php
@@ -0,0 +1,40 @@
+<?php
+require_once("../config.php");
+/*
+    Cerca i profili per nome o cognome
+*/
+
+class API {
+
+    function Get() {
+        $db = new Connect;
+        $records = array();
+        $content = strtolower(apache_request_headers()["Content"]);
+
+        $data = $db->prepare("SELECT Id, Nome, Cognome FROM Profilo");
+        $data->execute();
+
+        while($out = $data->fetch(PDO::FETCH_ASSOC)) {
+
+            $name = strtolower($out["Nome"]);
+            $surname = strtolower($out["Cognome"]);
+
+            if(strpos($name, $content) === 0 || strpos($surname, $content) === 0) {
+                $records[$out["Id"]] = array(
+                    "Id" => $out["Id"],
+                    "Nome" => $out["Nome"],
+                    "Cognome" => $out["Cognome"]
+                );
+            }
+        }
+
+        return json_encode($records);
+    }
+}
+
+$api = new API;
+header(Options::CROSS_ORIGIN);
+header('Content-Type: application/json');
+echo $api->Get();
+
+?>
