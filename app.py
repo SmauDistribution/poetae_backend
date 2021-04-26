5b3442b267a53b608b3c0ea5307163e27ad590e3
Daniel Smau <smaudaniel19@gmail.com>
2021-04-26 18:13:30 +0200

    Aggiunto opencv

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import flask
+from flask import request
+import cv2
+import numpy as np
 
 app = flask.Flask(__name__)
 
 
-@app.route("/translate")
+@app.route("/translate", methods=["POST"])
 def translate():
-    return "it works"
+    if(request.method == "POST"):
+        data = request.files["File"].read()  # Get the image byte data
+        img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+        cv2.imwrite("./test.png", img)
+        return flask.Response(status=200)
