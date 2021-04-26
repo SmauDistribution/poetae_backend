fabcc5dea8896e76243e749298bc7f31ba06478f
Daniel Smau <smaudaniel19@gmail.com>
2021-04-26 14:53:24 +0200

    Aggiunto l' endpoint '/poems/find'

---
+++ poems/find/index.php
@@ -0,0 +1,44 @@
+<?php
+require_once("../../config.php");
+/*
+    Ritorna tutti i poemi che hanno il contenuto, il titolo o la descrizione simile all Content
+*/
+
+class API {
+
+    private function Match($out, $content) {
+        $titolo = strtolower($out["Titolo"]);
+        $descr = strtolower($out["Descrizione"]);
+        $poem = strtolower($out["Contenuto"]);
+
+        return strpos($titolo, $content) !== false || strpos($descr, $content) !== false || strpos($poem, $content) !== false;
+    }
+
+    function Get() {
+        $db = new Connect;
+        $records = array();
+        $content = strtolower(apache_request_headers()["Content"]);
+
+        $data = $db->prepare("SELECT * FROM Poesia");
+        $data->execute();
+
+        while($out = $data->fetch(PDO::FETCH_ASSOC)) {
+                        
+            if($this->Match($out, $content)) {
+                $records[$out["Id"]] = array(
+                    "Id" => $out["Id"],
+                    "Titolo" => $out["Titolo"]
+                );
+            }
+        }
+
+        return json_encode($records);
+    }
+}
+
+$api = new API;
+header(Options::CROSS_ORIGIN);
+header('Content-Type: application/json');
+echo $api->Get();
+
+?>
