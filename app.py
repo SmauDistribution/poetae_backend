a9c6dd0d9f5a28e43969f9d3d54c2bcfdd326799
Daniel Smau <smaudaniel19@gmail.com>
2021-05-19 14:49:40 +0200

    Aggiunto e implementato easyocr

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import flask
 from flask import request
 import cv2
 import numpy as np
+import easyocr
 
 app = flask.Flask(__name__)
+reader = easyocr.Reader(['it'], gpu=False)
 
 
 @app.route("/translate", methods=["POST"])
@@ -11,5 +13,14 @@ def translate():
     if(request.method == "POST"):
         data = request.files["File"].read()  # Get the image byte data
         img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
-        cv2.imwrite("./test.png", img)
-        return flask.Response(status=200)
+        res = reader.readtext(img)
+        content = ""
+
+        for el in res:
+            content += el[1] + " "
+
+        resp = {
+            "Content": content
+        }
+
+        return resp
