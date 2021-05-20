25bceeb9a7b636372ce2757998583a3950e2810d
Daniel Smau <smaudaniel19@gmail.com>
2021-05-20 19:40:06 +0200

    Risolto problemi con il CORS

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 import flask
 from flask import request
+from flask import jsonify
+from flask_cors import CORS
 import cv2
 import numpy as np
 import easyocr
 
 app = flask.Flask(__name__)
+CORS(app)
 reader = easyocr.Reader(['it'], gpu=False)
+
+
+def get_format(filename):
+    fformat = filename.split(".")[1]
+    return fformat
 
 
 @app.route("/translate", methods=["POST"])
 def translate():
+    response = jsonify({})
+
     if(request.method == "POST"):
-        data = request.files["File"].read()  # Get the image byte data
-        img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
-        res = reader.readtext(img)
-        content = ""
+        ffile = request.files["File"]
+        fformat = get_format(ffile.filename)
 
-        for el in res:
-            content += el[1] + " "
+        if(fformat == 'png' or fformat == 'jpg'):
+            data = ffile.read()  # Get the image byte data
+            img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+            res = reader.readtext(img)
+            content = ""
 
-        resp = {
-            "Content": content
-        }
+            for el in res:
+                content += el[1] + " "
 
-        return resp
+            response = jsonify({
+                "Content": content
+            })
+        else:
+            return {}, 415
+
+    return response
